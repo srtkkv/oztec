790965854d9cda33d716b20c4251180a8c084432
kkovalenko <kkovalenko@swiftcom.uk>
2023-01-18 10:00:42 +0300

    start workin on #2 (comment)

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ class installer():
     def __init__(self,os_name):
         print(os_name)
         pass
+    def Windows_install(self):
+        pass
+    def mac_install(self):
+        pass
 
-
+class runner():
+    def __init__(self):
+        while True:
+            pass
+        pass
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(
@@ -22,5 +30,5 @@ if __name__=="__main__":
     args = parser.parse_args()
     print( args.service, args.verbose, args.service_name)
     if args.service:
-        print("asdaaaaaa")
+        print(sys.hash_info)
         i = installer(sys.platform)
